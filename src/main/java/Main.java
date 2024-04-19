e52f37c37589ae82b76abc2986bbc4f92a13991f
Mohammad Hossein Soleymani <hssoleymanmh@gmail.com>
2024-04-19 23:48:38 +0330

    Add protobuf to project

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,6 @@
-import java.time.LocalTime;
 
 public class Main {
     public static void main(String[] args) {
-        LocalTime localTime = LocalTime.now();
-        System.out.println(localTime);
+
     }
 }
